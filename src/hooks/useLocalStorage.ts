0d10e418a44139109d885216a256528b47e65ce2
ezdev2 <fansoaezra@gmail.com>
2023-06-15 09:47:20 +0300

    hook: create useLocalstorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,26 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from 'react';
 
-}
+const useLocalStorage = (key: string, initialValue: any) => {
+  // Utilisez useState pour gérer la valeur dans le composant
+  const [value, setValue] = useState(() => {
+    // Vérifiez si la clé existe dans le Local Storage
+    const storedValue = localStorage.getItem(key);
+    // Si la clé existe, retournez sa valeur
+    if (storedValue) {
+      return JSON.parse(storedValue);
+    }
+    // Sinon, retournez l'initialValue
+    return initialValue;
+  });
+
+  // Utilisez useEffect pour sauvegarder la valeur dans le Local Storage à chaque changement
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  // Retournez la valeur et une fonction pour mettre à jour la valeur
+  return [value, setValue];
+};
 
 export {
   useLocalStorage
